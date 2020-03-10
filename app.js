a0f7e9720601668b33df67279be739b0726247ab
linus muema <linus.m.muema@gmail.com>
2020-03-10 12:53:48 +0300

    Added the MDB cdn links

--- app.js
+++ app.js
@@ -2,14 +2,19 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
+const quiz_route = require('./routes/quizes')
+const add_route = require('./routes/add')
 const app = express()
-const route = require('./routes/quizes')
 const router = express.Router()
 const dbURI = "mongodb://localhost/quizes"
 
 //Application config
 app.use(express.json())
-app.use('/quizes', route)
+app.use('/quizes', quiz_route)
+app.use('/add', add_route)
+app.set('view engine', 'pug')
+app.set("views", path.join(__dirname, "views"));
 app.listen(process.env.PORT || 2400, (req, res) => {
     console.log('Server started : 2400')
 })
--- routes/add.js
+++ routes/add.js
@@ -1,4 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const pug = require('pug')
-const path = require('path')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.render('add')
+})
+
+module.exports = router
