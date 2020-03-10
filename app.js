5e83ab9012204c653c655d2c38ac1457d1cd0038
linus muema <linus.m.muema@gmail.com>
2020-03-10 11:58:55 +0300

    Added the routes and model script files

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 require('dotenv').config()
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
-const router = express.Router();
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const router = express.Router()
+
+app.use(express.json())
+app.listen(process.env.PORT || 2400, (req, res) => {
+    console.log('Server started : 2400')
+})
+
+module.exports = router
