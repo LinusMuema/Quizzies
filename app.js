6d12f0fc7719f10fd0acb2c09d617d9b311b4863
linus muema <linus.m.muema@gmail.com>
2020-03-12 15:01:41 +0300

    Added the input form

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
+const bodyParser = require('body-parser')
 const quiz_route = require('./routes/quizes')
 const add_route = require('./routes/add')
 const app = express()
@@ -13,6 +14,8 @@ const dbURI = "mongodb://localhost/quizes"
 app.use(express.json())
 app.use('/quizes', quiz_route)
 app.use('/add', add_route)
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.set('view engine', 'pug')
 app.set("views", path.join(__dirname, "views"));
 app.listen(process.env.PORT || 2400, (req, res) => {
--- routes/add.js
+++ routes/add.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const quizes = require('../models/quiz')
 const router = express.Router()
+const bodyParser = require('body-parser')
+const parser = bodyParser.urlencoded({extended: false});
 
-router.post('/', (req, res) => {
-    let quiz = new quizes({
-        question: req.body.question,
-        answers: req.body.answers
-    }).save()
-    .then((response) => {res.json({message: 'success', quiz: response})})
-    .catch((err) => {res.status(400).json({message: 'error', err})})
+router.get('/', (req, res) =>{
+    res.render('add')
+})
+
+router.post('/', parser, (req, res) => {
+    let question = req.body.question;
+    let option_one = `{"option":${req.body.option_one}, "value":${(req.body.answer_one == 'true')}}`
+    let option_two = `{"option":${req.body.option_two}, "value":${(req.body.answer_two == 'true')}}`
+    let option_three = `{"option":${req.body.option_three}, "value":${(req.body.answer_three == 'true')}}`
+    let option_four = `{"option":${req.body.option_four}, "value":${(req.body.answer_four == 'true')}}`
+    let answers = [option_one, option_two, option_three, option_four]
+    console.log(question, answers)
 })
 
 module.exports = router
