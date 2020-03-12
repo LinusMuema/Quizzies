54cbfab6485c3a3928c73546e73bd8167cde2895
linus muema <linus.m.muema@gmail.com>
2020-03-12 16:30:04 +0300

    Add the json parse function

--- routes/add.js
+++ routes/add.js
@@ -10,11 +10,13 @@ router.get('/', (req, res) =>{
 
 router.post('/', parser, (req, res) => {
     let question = req.body.question;
-    let option_one = `{"option":${req.body.option_one}, "value":${(req.body.answer_one == 'true')}}`
-    let option_two = `{"option":${req.body.option_two}, "value":${(req.body.answer_two == 'true')}}`
-    let option_three = `{"option":${req.body.option_three}, "value":${(req.body.answer_three == 'true')}}`
-    let option_four = `{"option":${req.body.option_four}, "value":${(req.body.answer_four == 'true')}}`
-    let answers = [option_one, option_two, option_three, option_four]
+    let option_one = `{"option":"${req.body.option_one}", "value":${(req.body.answer_one == 'true')}}`
+    let option_two = `{"option":"${req.body.option_two}", "value":${(req.body.answer_two == 'true')}}`
+    let option_three = `{"option":"${req.body.option_three}", "value":${(req.body.answer_three == 'true')}}`
+    let option_four = `{"option":"${req.body.option_four}", "value":${(req.body.answer_four == 'true')}}`
+    let answers = JSON.parse(`{"answers" : [${option_one}, ${option_two}, ${option_three}, ${option_four}]}`)
+    console.log(answers)
+    console.log(answers)
     let quiz = new quizes({
         question: question,
         answers: answers
